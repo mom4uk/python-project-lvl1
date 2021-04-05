e6e421acd1b4344366bb56363188dd8b391295db
mom4uk <imanovplus@gmail.com>
2021-04-05 22:13:02 +0300

    fix engine

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -10,7 +10,7 @@ def engine(descripsion, get_task_and_answer):
     print('Welcome to the Brain Games!')
     cycles_count = 3
     name = prompt.string('May I have your name? ')
-    message = "'{wrong}' is wrong answer ;(. Correct answer was '{correct}'."
+    message = "'{wrong}' is wrong answer ;(. Correct answer was '{correct}'.\n"
     print(f'Hello, {name}')
     print(descripsion)
     while cycles_count > 0:
@@ -18,7 +18,9 @@ def engine(descripsion, get_task_and_answer):
         print(f'Question: {question}')
         answer = prompt.string('Your answer: ')
         if answer != correct_answer:
-            print(message.format(wrong=answer, correct=correct_answer))
+            print(message.format(wrong=answer, correct=correct_answer)
+                + f'Let\'s try again, Bill!'
+            )
             return
         else:
             print('Correct!')
